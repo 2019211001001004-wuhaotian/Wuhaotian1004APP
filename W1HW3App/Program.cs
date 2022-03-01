923a2c3477258ca28f678b7e5127a0280c60d195
2019211001001004-wuhaotian <2636381952@qq.com>
2022-03-01 09:06:54 +0800

    添加项目文件。

---
+++ W1HW3App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW3App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入要判断的年份");
+            int year = Convert.ToInt32(Console.ReadLine());
+            bool result = (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
